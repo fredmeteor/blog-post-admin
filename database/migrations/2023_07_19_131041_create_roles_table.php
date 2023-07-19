bda4c09da7df69b6da9659e3cd151387691eaedb
fredmeteor <fredpeter09899@gmail.com>
2023-07-19 16:22:22 +0300

    Modified roles and role_user pivot tables

--- database/migrations/2023_07_19_131041_create_roles_table.php
+++ database/migrations/2023_07_19_131041_create_roles_table.php
@@ -12,8 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('roles', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->increments('id');
+ $table->string('name');
+ $table->timestamps();
         });
     }
 
