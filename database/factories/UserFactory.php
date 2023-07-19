0a6ad8968c182976ad9250fbcfa1fe8c831a3193
fredmeteor <fredpeter09899@gmail.com>
2023-07-19 17:50:35 +0300

    Added UserController and UserSeeder

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -4,12 +4,14 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
-
+use App\Models\User;
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
  */
 class UserFactory extends Factory
 {
+
+    protected $model = \App\Models\User::class;
     /**
      * Define the model's default state.
      *
@@ -24,6 +26,9 @@ class UserFactory extends Factory
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
         ];
+
+        
+ 
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\models\User;
+
+use App\Http\Controllers\UserController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +21,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('users' , [UserController::class, 'index']);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
